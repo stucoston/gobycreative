092e5a00a3bd96f79ec16cfb4b714cd6b1b95b07
Stu Coston <stu.coston@gmail.com>
2017-09-10 16:25:24 -0600

    Update youtube-audio.js

--- beckybullerband/youtube-audio.js
+++ beckybullerband/youtube-audio.js
@@ -9,102 +9,135 @@
 */
 
 function onYouTubeIframeAPIReady(){
-  var 1=document.getElementById("youtube-audio-1"),
-  2=document.getElementById("youtube-audio-2"),
-  3=document.getElementById("youtube-audio-3"),
-  4=document.getElementById("youtube-audio-4"),
-  5=document.getElementById("youtube-audio-5"),
-  6=document.getElementById("youtube-audio-6"),
-  7=document.getElementById("youtube-audio-7"),
-  8=document.getElementById("youtube-audio-8"),
-  9=document.getElementById("youtube-audio-9"),
-  10=document.getElementById("youtube-audio-10"),
-  11=document.getElementById("youtube-audio-11"),
-  12=document.getElementById("youtube-audio-12"),
+  var a=document.getElementById("youtube-audio-1"),
+  b=document.getElementById("youtube-audio-2"),
+  c=document.getElementById("youtube-audio-3"),
+  d=document.getElementById("youtube-audio-4"),
+  e=document.getElementById("youtube-audio-5"),
+  f=document.getElementById("youtube-audio-6"),
+  g=document.getElementById("youtube-audio-7"),
+  h=document.getElementById("youtube-audio-8"),
+  i=document.getElementById("youtube-audio-9"),
+  j=document.getElementById("youtube-audio-10"),
+  k=document.getElementById("youtube-audio-11"),
+  l=document.getElementById("youtube-audio-12"),
   
   t=document.createElement("img");
   t.setAttribute("id","youtube-icon"),
   t.style.cssText="cursor:pointer;cursor:hand",
   
-  1.appendChild(t);
-  2.appendChild(t);
-  3.appendChild(t);
-  4.appendChild(t);
-  5.appendChild(t);
-  6.appendChild(t);
-  7.appendChild(t);
-  8.appendChild(t);
-  9.appendChild(t);
-  10.appendChild(t);
-  11.appendChild(t);
-  12.appendChild(t);
+  a.appendChild(t);
+  b.appendChild(t);
+  c.appendChild(t);
+  d.appendChild(t);
+  e.appendChild(t);
+  f.appendChild(t);
+  g.appendChild(t);
+  h.appendChild(t);
+  i.appendChild(t);
+  j.appendChild(t);
+  k.appendChild(t);
+  l.appendChild(t);
   
-  var a=document.createElement("div");
-  a.setAttribute("id","youtube-player"),
+  var m=document.createElement("div");
+  m.setAttribute("id","youtube-player"),
   
-  1.appendChild(a);
-  2.appendChild(a);
-  3.appendChild(a);
-  4.appendChild(a);
-  5.appendChild(a);
-  6.appendChild(a);
-  7.appendChild(a);
-  8.appendChild(a);
-  9.appendChild(a);
-  10.appendChild(a);
-  11.appendChild(a);
-  12.appendChild(a);
+  a.appendChild(m);
+  b.appendChild(m);
+  c.appendChild(m);
+  d.appendChild(m);
+  e.appendChild(m);
+  f.appendChild(m);
+  g.appendChild(m);
+  h.appendChild(m);
+  i.appendChild(m);
+  j.appendChild(m);
+  k.appendChild(m);
+  l.appendChild(m);
   
-  var o=function(e){
-    var a=1?"IDzX9gL.png":"quyUPXN.png",
-    a=2?"IDzX9gL.png":"quyUPXN.png",
-    a=3?"IDzX9gL.png":"quyUPXN.png",
-    a=4?"IDzX9gL.png":"quyUPXN.png",
-    a=5?"IDzX9gL.png":"quyUPXN.png",
-    a=6?"IDzX9gL.png":"quyUPXN.png",
-    a=7?"IDzX9gL.png":"quyUPXN.png",
-    a=8?"IDzX9gL.png":"quyUPXN.png",
-    a=9?"IDzX9gL.png":"quyUPXN.png",
-    a=10?"IDzX9gL.png":"quyUPXN.png",
-    a=11?"IDzX9gL.png":"quyUPXN.png",
-    a=12?"IDzX9gL.png":"quyUPXN.png";
-    t.setAttribute("src","https://i.imgur.com/"+a)
+  var n=function(a){
+    var m=a?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var o=function(b){
+    var m=b?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var p=function(c){
+    var m=c?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var q=function(d){
+    var m=d?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var s=function(e){
+    var m=e?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var u=function(f){
+    var m=f?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var v=function(g){
+    var m=g?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var w=function(h){
+    var m=h?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var x=function(i){
+    var m=i?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var y=function(j){
+    var m=j?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var z=function(k){
+    var m=k?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
+  };
+ var aa=function(l){
+    var m=l?"IDzX9gL.png":"quyUPXN.png",
+    t.setAttribute("src","https://i.imgur.com/"+m)
   };
   
-  1.onclick=function(){
+  a.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  2.onclick=function(){
+  b.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  3.onclick=function(){
+  c.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  4.onclick=function(){
+  d.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  5.onclick=function(){
+  e.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  6.onclick=function(){
+  f.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  7.onclick=function(){
+  g.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  8.onclick=function(){
+  h.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  9.onclick=function(){
+  i.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  10.onclick=function(){
+  j.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  11.onclick=function(){
+  k.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
-  12.onclick=function(){
+  l.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
   
@@ -113,118 +146,118 @@ function onYouTubeIframeAPIReady(){
     width:"0",
     videoId:e.dataset.video,
     playerVars:{
-      autoplay:1.dataset.autoplay,
-      autoplay:2.dataset.autoplay,
-      autoplay:3.dataset.autoplay,
-      autoplay:4.dataset.autoplay,
-      autoplay:5.dataset.autoplay,
-      autoplay:6.dataset.autoplay,
-      autoplay:7.dataset.autoplay,
-      autoplay:8.dataset.autoplay,
-      autoplay:9.dataset.autoplay,
-      autoplay:10.dataset.autoplay,
-      autoplay:11.dataset.autoplay,
-      autoplay:12.dataset.autoplay,
+      autoplay:a.dataset.autoplay,
+      autoplay:b.dataset.autoplay,
+      autoplay:c.dataset.autoplay,
+      autoplay:d.dataset.autoplay,
+      autoplay:e.dataset.autoplay,
+      autoplay:f.dataset.autoplay,
+      autoplay:g.dataset.autoplay,
+      autoplay:h.dataset.autoplay,
+      autoplay:i.dataset.autoplay,
+      autoplay:k.dataset.autoplay,
+      autoplay:k.dataset.autoplay,
+      autoplay:l.dataset.autoplay,
       
-      loop:1.dataset.loop,
-      loop:2.dataset.loop,
-      loop:3.dataset.loop,
-      loop:4.dataset.loop,
-      loop:5.dataset.loop,
-      loop:6.dataset.loop,
-      loop:7.dataset.loop,
-      loop:8.dataset.loop,
-      loop:9.dataset.loop,
-      loop:10.dataset.loop,
-      loop:11.dataset.loop,
-      loop:12.dataset.loop
+      loop:a.dataset.loop,
+      loop:b.dataset.loop,
+      loop:c.dataset.loop,
+      loop:d.dataset.loop,
+      loop:e.dataset.loop,
+      loop:f.dataset.loop,
+      loop:g.dataset.loop,
+      loop:h.dataset.loop,
+      loop:i.dataset.loop,
+      loop:j.dataset.loop,
+      loop:k.dataset.loop,
+      loop:l.dataset.loop
     },
     events:{
     
-      onReady:function(1){
+      onReady:function(a){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(2){
+      onReady:function(b){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(3){
+      onReady:function(c){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(4){
+      onReady:function(d){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(5){
+      onReady:function(e){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(6){
+      onReady:function(f){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(7){
+      onReady:function(g){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(8){
+      onReady:function(h){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(9){
+      onReady:function(i){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(10){
+      onReady:function(j){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(11){
+      onReady:function(k){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onReady:function(12){
+      onReady:function(l){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
       
-      onStateChange:function(1){
-        1.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(a){
+        a.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(2){
-        2.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(b){
+        b.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(3){
-        3.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(c){
+        c.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(4){
-        4.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(d){
+        d.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(5){
-        5.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(e){
+        e.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(6){
-        6.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(f){
+        f.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(7){
-        7.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(g){
+        g.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(8){
-        8.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(h){
+        h.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(9){
-        9.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(i){
+        i.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(10){
-        10.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(j){
+        j.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(11){
-        11.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(k){
+        k.data===YT.PlayerState.ENDED&&o(!1)
       }
-      onStateChange:function(12){
-        12.data===YT.PlayerState.ENDED&&o(!1)
+      onStateChange:function(l){
+        l.data===YT.PlayerState.ENDED&&o(!1)
       }
     }
   })
