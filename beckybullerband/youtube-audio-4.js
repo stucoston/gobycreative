fa89a238483f2189dfee2b9808072e2bdd8c9654
Stu Coston <stu.coston@gmail.com>
2017-09-10 19:24:41 -0600

    Update youtube-audio-4.js

--- beckybullerband/youtube-audio-4.js
+++ beckybullerband/youtube-audio-4.js
@@ -9,39 +9,39 @@
 */
 
 function onYouTubeIframeAPIReady(){
-  var e=document.getElementById("youtube-audio-4"),
-  
+  var f=document.getElementById("youtube-audio-4"),
   t=document.createElement("img");
+ 
   t.setAttribute("id","youtube-icon"),
   t.style.cssText="cursor:pointer;cursor:hand",
+  f.appendChild(t);
   
-  e.appendChild(t);
+  var b=document.createElement("div");
+  b.setAttribute("id","youtube-player"),
   
-  var a=document.createElement("div");
-  a.setAttribute("id","youtube-player"),
+  f.appendChild(b);
   
-  e.appendChild(a);
-  
-  var o=function(e){
-    var a=e?"IDzX9gL.png":"quyUPXN.png";
+  var o=function(f){
+    var a=f?"IDzX9gL.png":"quyUPXN.png";
     t.setAttribute("src","https://i.imgur.com/"+a)
   };
-  e.onclick=function(){
+  f.onclick=function(){
     r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))
   };
   var r=new YT.Player("youtube-player",{
     height:"0",
     width:"0",
-    videoId:e.dataset.video,
+    videoId:f.dataset.video,
     playerVars:{
-      autoplay:e.dataset.autoplay,loop:e.dataset.loop
+      autoplay:f.dataset.autoplay,
+      loop:f.dataset.loop
     },
     events:{
-      onReady:function(e){
+      onReady:function(f){
         r.setPlaybackQuality("small"),
         o(r.getPlayerState()!==YT.PlayerState.CUED)
       },
-      onStateChange:function(e){
+      onStateChange:function(f){
         e.data===YT.PlayerState.ENDED&&o(!1)
       }
     }
